6a29b803e925602918c5fcbc0ef2c8af84390240
olakayCoder1 <programmerolakay@gmail.com>
2024-06-13 16:51:37 +0100

    feat:application testing

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 from rest_framework.test import APIClient
 from .models import Item, Supplier
 
@@ -10,9 +11,11 @@ class InventoryAPITests(TestCase):
         self.supplier = Supplier.objects.create(name="Test Supplier", contact_info="1234567890")
         self.item = Item.objects.create(name="Test Item", description="Test Description", price=10.0)
         self.item.suppliers.add(self.supplier)
+        
 
     def test_create_item(self):
-        response = self.client.post('/api/items/', {
+        url = reverse('item-list')
+        response = self.client.post(url, {
             'name': 'New Item',
             'description': 'New Description',
             'price': 15.0,
@@ -20,17 +23,55 @@ class InventoryAPITests(TestCase):
         })
         self.assertEqual(response.status_code, 201)
 
+    def test_update_item(self):
+        url = reverse('item-detail', args=[self.item.id])
+        response = self.client.put(url, {
+            'name': 'Updated Item',
+            'description': 'Updated Description',
+            'price': 20.0,
+            'suppliers': [self.supplier.id]
+        })
+        self.assertEqual(response.status_code, 200)
+        self.item.refresh_from_db()
+        self.assertEqual(self.item.name, 'Updated Item')
+
     def test_get_items(self):
-        response = self.client.get('/api/items/')
+        url = reverse('item-list')
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_create_supplier(self):
-        response = self.client.post('/api/suppliers/', {
+        url = reverse('supplier-list')
+        response = self.client.post(url, {
             'name': 'New Supplier',
             'contact_info': '9876543210'
         })
         self.assertEqual(response.status_code, 201)
 
-    def test_get_suppliers(self):
-        response = self.client.get('/api/suppliers/')
+    def test_update_supplier(self):
+        url = reverse('supplier-detail', args=[self.supplier.id])
+        response = self.client.put(url, {
+            'name': 'Updated Supplier',
+            'contact_info': '9999999999'
+        })
         self.assertEqual(response.status_code, 200)
+        self.supplier.refresh_from_db()
+        self.assertEqual(self.supplier.name, 'Updated Supplier')
+
+    def test_get_suppliers(self):
+        url = reverse('supplier-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_suppliers_for_item(self):
+        url = reverse('item-suppliers', args=[self.item.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
+
+
+    def test_get_items_for_supplier(self):
+        url = reverse('supplier-items', args=[self.supplier.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,8 @@
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from rest_framework import viewsets, filters
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from inventory.pagination import StandardResultsSetPagination
 from .models import Item, Supplier
 from .serializers import ItemSerializer, SupplierSerializer
@@ -18,6 +20,14 @@ class ItemViewSet(viewsets.ModelViewSet):
     @method_decorator(cache_page(30*1)) 
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
+    
+
+    @action(detail=True, methods=['get'])
+    def suppliers(self, request, pk=None):
+        item = self.get_object()
+        suppliers = item.suppliers.all()
+        serializer = SupplierSerializer(suppliers, many=True)
+        return Response(serializer.data)
 
 
 
@@ -33,3 +43,11 @@ class SupplierViewSet(viewsets.ModelViewSet):
     @method_decorator(cache_page(30*1))
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
+    
+
+    @action(detail=True, methods=['get'])
+    def items(self, request, pk=None):
+        supplier = self.get_object()
+        items = supplier.items.all()  # Assuming 'items' is a related_name in your Supplier model
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)
